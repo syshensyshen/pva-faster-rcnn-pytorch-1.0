31186f0902cb90c8896177575db7c15d33cec594
syshensyshen <syshensyshen@hotmail.com>
2019-01-10 21:59:29 +0800

    load datasets

---
+++ lib/datasets/pascal_voc.py
@@ -0,0 +1,151 @@
+import os
+import xml.etree.ElementTree as ET
+import numpy as np
+import scipy.sparse
+import scipy.io as sio
+import subprocess
+import uuid
+import cv2
+import random
+
+SCALES = (416, 448, 480, 512, 544, 576, 608, 640, 672, 704, 736, 768, 800, 832, 864)
+PIXEL_MEANS = np.array([[[102.9801, 115.9465, 122.7717]]])
+SCALE_MULTIPLE_OF = 32
+MAX_SIZE = 1440
+
+
+#class_list = ('__background__', 'left', 'right', 'top','bottom', 'middle', 'back', 'label', 'lable')
+class_list = ('__background__', # always index 0
+                         'aeroplane', 'bicycle', 'bird', 'boat',
+                         'bottle', 'bus', 'car', 'cat', 'chair',
+                         'cow', 'diningtable', 'dog', 'horse',
+                         'motorbike', 'person', 'pottedplant',
+                         'sheep', 'sofa', 'train', 'tvmonitor')
+class_to_ind = dict(zip(class_list, range(0, len(class_list)-1)))
+
+def load_pascal_annotation(xml_path):
+    if not os.path.isfile(xml_path):
+        print('xml file not exists!\r\n')
+    tree = ET.parse(xml_path)
+    img_name = tree.find('filename').text.strip()
+    objs = tree.findall('object')
+    num_objs = len(objs)
+    boxes = np.zeros((num_objs, 4), dtype=np.uint16)
+    gt_classes = np.zeros((num_objs), dtype=np.int32)
+    overlaps = np.zeros((num_objs, len(class_list)-1), dtype=np.float32)
+    # "Seg" area for pascal is just the box area
+    seg_areas = np.zeros((num_objs), dtype=np.float32)
+    
+    # Load object bounding boxes into a data frame.
+    for ix, obj in enumerate(objs):
+        #print(obj.find('name').text.strip)
+        bbox = obj.find('bndbox')
+        # Make pixel indexes 0-based
+        x1 = float(bbox.find('xmin').text) #- 1
+        y1 = float(bbox.find('ymin').text) #- 1
+        x2 = float(bbox.find('xmax').text) #- 1
+        y2 = float(bbox.find('ymax').text) #- 1
+        #cls = self._class_to_ind[obj.find('name').text.lower().strip()]        
+        class_name = obj.find('name').text.strip()
+        if not class_name in class_list:
+            continue
+        cls = class_to_ind[class_name]
+        boxes[ix, :] = [x1, y1, x2, y2]
+        gt_classes[ix] = cls
+        overlaps[ix, cls] = 1.0
+        seg_areas[ix] = (x2 - x1 + 1) * (y2 - y1 + 1)
+
+    overlaps = scipy.sparse.csr_matrix(overlaps)
+    zeros_label = []
+    for ix, label in enumerate(gt_classes):
+        if label == 0:
+            zeros_label.append(ix)
+    #print boxes, gt_classes
+    boxes = np.delete(boxes, zeros_label, axis=0)
+    gt_classes = np.delete(gt_classes, zeros_label, axis=0)
+    #print boxes, gt_classes
+    #raw_input()
+
+    return {'img_name': img_name,
+            'boxes' : boxes,
+            'gt_classes': gt_classes,
+            'gt_overlaps' : overlaps,
+            'flipped' : False,
+            'seg_areas' : seg_areas}
+
+def prep_im_for_blob(im, pixel_means, target_size, max_size, multiple):
+    """Mean subtract and scale an image for use in a blob."""
+    im = im.astype(np.float32, copy=False)
+    im -= pixel_means
+    im_shape = im.shape
+    im_size_min = np.min(im_shape[0:2])
+    im_size_max = np.max(im_shape[0:2])
+    im_scale = float(target_size) / float(im_size_min)
+    # Prevent the biggest axis from being more than MAX_SIZE
+    if np.round(im_scale * im_size_max) > max_size:
+        im_scale = float(max_size) / float(im_size_max)
+    im_scale_x = np.floor(im.shape[1] * im_scale / multiple) * multiple / im.shape[1]
+    im_scale_y = np.floor(im.shape[0] * im_scale / multiple) * multiple / im.shape[0]
+    im = cv2.resize(im, None, None, fx=im_scale_x, fy=im_scale_y,
+                    interpolation=cv2.INTER_LINEAR)
+    return im, np.array([im_scale_x, im_scale_y])
+
+def im_list_to_blob(im):
+   
+    blob = np.zeros((1, im.shape[0], im.shape[1], 3),
+                    dtype=np.float32)
+    blob[0, 0:im.shape[0], 0:im.shape[1], :] = im
+    # Move channels (axis 3) to axis 1
+    # Axis order will become: (batch elem, channel, height, width)
+    channel_swap = (0, 3, 1, 2)
+    blob = blob.transpose(channel_swap)
+    return blob
+
+def _get_image_blob(im, PIXEL_MEANS, target_size, MAX_SIZE, SCALE_MULTIPLE_OF):
+    """Builds an input blob from the images in the roidb at the specified
+    scales.
+    """
+    #from sample_argument_tools import lightexchange
+    #im = cv2.imread(img_path)
+    im, im_scale = prep_im_for_blob(im, PIXEL_MEANS, target_size,
+                                        MAX_SIZE, SCALE_MULTIPLE_OF)
+    # Create a blob to hold the input images
+    blob = np.zeros((im.shape[0], im.shape[1], 3),
+                    dtype=np.float32)
+    blob[0:im.shape[0], 0:im.shape[1], :] = im
+    
+    # Move channels (axis 3) to axis 1
+    # Axis order will become: (batch elem, channel, height, width)
+    channel_swap = (2, 0, 1)
+    blob = blob.transpose(channel_swap)
+    print(blob.shape)
+
+    return blob, im_scale 
+
+def prepareBatchData(batch_size, xmllist):
+    #for ix, xml_path in enumerate(xmllist):
+    ims = []
+    boxes = []
+    labels = []
+    max_len = 0
+    for xml_path in xmllist:
+        xml_context = load_pascal_annotation(xml_path)
+        if max_len < xml_context['boxes'].shape[0]:
+            max_len = xml_context['boxes'].shape[0]
+        ims.append(cv2.imread(xml_context['img_name']))
+        boxes.append(xml_context['boxes'])
+        labels.append(xml_context['gt_classes'])
+    gt_boxes = np.zeros((batch_size, max_len, 5), dtype=np.float32)
+    im_blobs = np.zeros((batch_size, ims[0].shape[2], ims[0].shape[0], ims[0].shape[1]), dtype = np.float32)
+    im_scales = np.zeros((batch_size, 3), dtype = np.float32)
+    target_size = SCALES[random.randint(0, len(SCALES))]
+    for ix, gt in boxes:
+        len_t = gt.shape[0]
+        im_blobs[ix, :, :, :], im_scale = _get_image_blob(ims[ix], PIXEL_MEANS, target_size, MAX_SIZE, SCALE_MULTIPLE_OF)
+        gt_boxes[ix, 0:len_t, 0:4] = gt * im_scale[0]
+        gt_boxes[ix, 0:len_t, 4:5] = labels[ix]
+        im_scales[ix,:] = np.array(
+            [np.hstack((im_blobs[ix, :, :, :].shape[2], im_blobs[ix, :, :, :].shape[3], im_scales[0]))],
+            dtype=np.float32)
+    
+    return gt_boxes, im_blobs, im_scales
--- test-syshen.py
+++ test-syshen.py
@@ -1,10 +1,66 @@
 from __future__ import absolute_import
-from models.pvanet import PVANetFeat
-from models.lite import PVALiteFeat
+from models.features import pvaHyper
+from models.features import liteHyper
+from models.lite import PVALiteNet
 import torch
 from torchsummary import summary
-
+import cv2
+import numpy as np
+'''
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-model = PVALiteFeat().to(device)
-summary(model, (3, 224, 224))
+#model = liteHyper().to(device)
+#model = pvaHyper().to(device)
+img_size = 192
+num_classes = 1000
+batch_size = 12
+model = PVALiteNet(inputsize=img_size, num_classes = num_classes).to(device)
+input = torch.empty(batch_size, 3, img_size, img_size, dtype=torch.float32).to(device)
+features = model.forward(input)
+print(features)
+print(features.shape)
+#summary(model, (3, 320, 320))
 #print(model)
+'''
+'''
+_feat_stride = 16
+width = 40
+height = 40
+import numpy as np
+shift_x = np.arange(0, width) * _feat_stride
+shift_y = np.arange(0, height) * _feat_stride
+shift_x, shift_y = np.meshgrid(shift_x, shift_y)
+print(shift_x.shape)
+print(shift_y.shape)
+import torch
+shifts = torch.from_numpy(np.vstack((shift_x.ravel(), shift_y.ravel(),
+                                  shift_x.ravel(), shift_y.ravel())).transpose())
+#shifts = shifts.contiguous().type_as(rpn_cls_score).float()
+print(shifts.shape)
+from lib.rpn.generate_anchors import generate_anchors
+
+# Anchor scales for RPN
+scales = [8,16,32]
+# Anchor ratios for RPN
+ratios = [0.5,1,2]
+anchors = torch.from_numpy(generate_anchors(scales=np.array(scales), ratios=np.array(ratios))).float()
+print(anchors.size(0))
+a=anchors.new(4, anchors.size(0))
+print(a.shape)
+batch_size = 4
+N = 9
+banchors = anchors.view(1, N, 4).expand(batch_size, N, 4).contiguous()
+print(banchors.shape)
+'''
+
+from lib.datasets.pascal_voc import load_pascal_annotation
+from lib.datasets.pascal_voc import _get_image_blob
+xml_context = load_pascal_annotation(r'E:\VOCdevkit\VOC2007\Annotations\000001.xml')
+print(xml_context['boxes'].shape[0])
+test_gt = np.zeros((2, 4, 5), dtype=np.float32)
+print(test_gt[0,0:2,0:3])
+img = cv2.imread(r"E:\VOCdevkit\VOC2007\JPEGImages\000001.jpg")
+print(img.shape)
+blob, im_scale = _get_image_blob(img, np.array([[[102.9801, 115.9465, 122.7717]]]), 512, 1440, 32)
+print(blob.shape, im_scale)
+cv2.imshow('img', img)
+cv2.waitKey(0)
