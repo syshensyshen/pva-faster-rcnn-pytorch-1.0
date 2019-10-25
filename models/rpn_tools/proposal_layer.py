0340c7c69bd3d0972a521a4cea390ff6d5f9f139
shengyuan <syshensyshen@hotmail.com>
2019-10-25 09:42:49 +0800

    Update proposal_layer.py

--- models/rpn_tools/proposal_layer.py
+++ models/rpn_tools/proposal_layer.py
@@ -32,13 +32,51 @@ class ProposalLayer(nn.Module):
         self.min_size = min_size
         self._feat_stride = feat_stride
 
+    def bbox_transform_inv(self, boxes, deltas):
+        widths = boxes[:, 2] - boxes[:, 0] + 1.0
+        heights = boxes[:, 3] - boxes[:, 1] + 1.0
+        ctr_x = boxes[:, 0] + 0.5 * widths
+        ctr_y = boxes[:, 1] + 0.5 * heights
+
+        dx = deltas[:, 0::4]
+        dy = deltas[:, 1::4]
+        dw = deltas[:, 2::4]
+        dh = deltas[:, 3::4]
+
+        pred_ctr_x = dx * widths.unsqueeze(1) + ctr_x.unsqueeze(1)
+        pred_ctr_y = dy * heights.unsqueeze(1) + ctr_y.unsqueeze(1)
+        pred_w = torch.exp(dw) * widths.unsqueeze(1)
+        pred_h = torch.exp(dh) * heights.unsqueeze(1)
+
+        pred_boxes = deltas.clone()
+        # x1
+        pred_boxes[:, 0::4] = pred_ctr_x - 0.5 * pred_w
+        # y1
+        pred_boxes[:, 1::4] = pred_ctr_y - 0.5 * pred_h
+        # x2
+        pred_boxes[:, 2::4] = pred_ctr_x + 0.5 * pred_w
+        # y2
+        pred_boxes[:, 3::4] = pred_ctr_y + 0.5 * pred_h
+
+        return pred_boxes
+
+    def clip_boxes(self, boxes, im_shape):
+
+        boxes[:, 0::4].clamp_(0, im_shape[1] - 1)
+        boxes[:, 1::4].clamp_(0, im_shape[0] - 1)
+        boxes[:, 2::4].clamp_(0, im_shape[1] - 1)
+        boxes[:, 3::4].clamp_(0, im_shape[0] - 1)
+
+        return boxes
+
     def forward(self, scores, bbox_deltas, anchors, num_anchors, im_info, pre_nms, post_nms, nms_thresh):
 
         batch_size = bbox_deltas.size(0)
 
         batch_size, feat_height, feat_width = scores.size(0), scores.size(2), scores.size(3)
 
-        anchors = anchors.reshape(1, -1, 4).expand(batch_size, -1, 4)
+        # anchors = anchors.reshape(1, -1, 4).expand(batch_size, -1, 4)
+        anchors = anchors.reshape(-1, 4)
 
         # Transpose and reshape predicted bbox transformations to get them
         # into the same order as the anchors:
@@ -50,46 +88,42 @@ class ProposalLayer(nn.Module):
         scores =  scores.reshape(batch_size, num_anchors *2 , -1, feat_width)[:, num_anchors:, :, :]
         scores = scores.permute(0, 2, 3, 1).reshape(batch_size, -1)
 
-        # Convert anchors into proposals via bbox transformations
-        proposals = bbox_transform_inv(anchors, bbox_deltas, batch_size)
-
-        # 2. clip predicted boxes to image
-        proposals = clip_boxes(proposals, im_info, batch_size)
-
-        # scores_keep = scores
-        # proposals_keep = proposals
-        # _, order = torch.sort(scores, 1, True)
         output = torch.zeros((batch_size, post_nms, 5)).to(scores.device)
 
         for i in range(batch_size):
 
-            proposals_single = proposals[i]
-            scores_single = scores[i]
+            # Convert anchors into proposals via bbox transformations
+            proposal = self.bbox_transform_inv(anchors, bbox_deltas[i])
 
-            keep = self._filter_boxes(proposals_single, self.min_size, im_info[i])
+            # 2. clip predicted boxes to image
+            proposal = self.clip_boxes(proposal, im_info[i])
+
+            score = scores[i]
+
+            keep = self._filter_boxes(proposal, self.min_size, im_info[i])
             # #print(proposals_single.shape, keep.shape)
-            proposals_single = proposals_single[keep]
-            scores_single = scores_single[keep]
-            _, order_single = torch.sort(scores_single, 0, True)
+            proposal = proposal[keep]
+            score = score[keep]
+            _, order = torch.sort(score, 0, True)
 
-            if pre_nms < scores_single.size(0):
-                order_single = order_single[:pre_nms]
+            if pre_nms < score.size(0):
+                order_single = order[:pre_nms]
 
-            proposals_single = proposals_single[order_single]
-            scores_single = scores_single[order_single].reshape(-1)
+            proposal = proposal[order]
+            score = score[order].reshape(-1)
 
-            keep_idx_i = nms(proposals_single, scores_single, nms_thresh).long()
+            keep_idx = nms(proposal, score, nms_thresh).long()
 
             if post_nms > 0:
-                keep_idx_i = keep_idx_i[:post_nms]
-            proposals_single = proposals_single[keep_idx_i]
-            scores_single = scores_single[keep_idx_i]
+                keep_idx = keep_idx[:post_nms]
+            proposal = proposal[keep_idx]
+            score = score[keep_idx]
 
-            num_proposal = proposals_single.size(0)
+            num_proposal = proposal.size(0)
             output[i, :, 0] = i
-            output[i, :num_proposal, 1:] = proposals_single
+            output[i, :num_proposal, 1:] = proposal
 
-        return output, proposals
+        return output
 
     def backward(self, top, propagate_down, bottom):
         """This layer does not propagate gradients."""
